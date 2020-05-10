daaecb70d440745b1cb22254f0bb19c36f4bbbdb
smanov8320 <61931343+smanov8320@users.noreply.github.com>
2020-05-10 13:22:36 +0600

    рабочие скилы

--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -1,1 +1,46 @@
-console.log('this is skills module');
+import Vue from "vue";
+
+const skill = {
+  template: "#skill",
+  props: ["skill"],
+  methods: {
+    drawColoredCircle() {
+      const circle = this.$refs["circle"];
+      const dashArray = parseInt(
+        getComputedStyle(circle).getPropertyValue("stroke-dasharray")
+      );
+      const percent = (dashArray / 100) * (100 - this.skill.percent);
+
+      circle.style.strokeDashoffset = percent;
+
+    },
+  },
+  mounted() {
+    this.drawColoredCircle();
+  },
+};
+
+const skillsRow = {
+  template: "#skills-row",
+  components: {
+    skill,
+  },
+  props: ["category"],
+};
+
+new Vue({
+  el: "#skills-component",
+  template: "#skills-list",
+  components: {
+    skillsRow,
+  },
+  data() {
+    return {
+      skills: [],
+    };
+  },
+  created() {
+    const data = require("../data/skills.json");
+    this.skills = data;
+  },
+});
