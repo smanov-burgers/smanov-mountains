460a9a33f1e50595545a9b40529725cd4cd55c99
smanov8320 <61931343+smanov8320@users.noreply.github.com>
2020-06-02 20:04:10 +0600

    gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = (env, argv) => {
   const isProductionBuild = argv.mode === "production";
-  const publicPath = './';
+  const publicPath = 'https://smanov-burgers.github.io/admin/';
 
   const pcss = {
     test: /\.(p|post|)css$/,
